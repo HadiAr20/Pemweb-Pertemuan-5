9a8a6e1c55cbf5c1602eb3fde43006cfe5928b12
Hadi Arsa <87856332+HadiAr20@users.noreply.github.com>
2022-11-14 16:41:42 +0700

    Add files via upload

---
+++ Pertemuan5-main/mahasiswa.sql
@@ -0,0 +1,56 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1:3306
+-- Generation Time: Nov 11, 2022 at 08:33 AM
+-- Server version: 5.7.36
+-- PHP Version: 7.4.26
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `db_tm5`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `mahasiswa`
+--
+
+DROP TABLE IF EXISTS `mahasiswa`;
+CREATE TABLE IF NOT EXISTS `mahasiswa` (
+  `nim` bigint(20) NOT NULL,
+  `nama` varchar(50) NOT NULL,
+  `prodi` varchar(50) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `mahasiswa`
+--
+
+INSERT INTO `mahasiswa` (`nim`, `nama`, `prodi`) VALUES
+(120140150, 'Muhammad Hadi Arsa', 'IF'),
+(120140154, 'Ryan Ernanda', 'IF'),
+(120140155, 'Dean Andhika Ramadhan', 'IF'),
+(120140059, 'Indra Jaya Putra', 'IF'),
+(120140145, 'Muhammad Nuur Arinda Marrezka', 'IF'),
+(120140141, 'Bilhaq Avi Dewantara', 'IF'),
+(120130008, 'Icha Sitorus', 'EL'),
+(120130020, 'Adam Kesuma Wijaya', 'EL'),
+(121140071, 'Maharani Triza Putri', 'IF'),
+(121140116, 'Retri Dela', 'IF');
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
